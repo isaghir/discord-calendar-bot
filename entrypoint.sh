e3d1224d501abc475d53547e44bfc835d6d3beb6
isaghir <68078865+isaghir@users.noreply.github.com>
2021-04-11 23:24:31 +0100

    edited comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+# This is a startup script that Fargate executes when running the container.
 
-# This is a startup script that Fargate executes when running the container
+set -ex #sets bash scripting flags to show log lines and to exit on command failure
 
-set -ex # sets bash scripting flags to show log lines and to exit on command failure
-
-python bot.py
+python bot.py #runs the bot.py file
 
